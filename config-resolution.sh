04353340c3e9cafe6b7d84faf33928715db7c020
Qiang Zhao <koqizhao@outlook.com>
2018-02-16 22:37:12 +0800

    add xrandr resolution config script

---
+++ config-resolution.sh
@@ -0,0 +1,5 @@
+
+xrandr --newmode 1680x1050_75.00  187.00  1680 1800 1976 2272  1050 1053 1059 1099 -hsync +vsync
+xrandr --addmode eDP-1 1680x1050_75.00
+xrandr --output eDP-1 --mode 1680x1050_75.00
+
