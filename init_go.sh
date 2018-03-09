5bf3f4086bea71e1908fcb7da6a1b8a45900c2f9
Qiang Zhao <koqizhao@outlook.com>
2018-03-09 09:47:51 +0800

    update init_go.sh

--- init_go.sh
+++ init_go.sh
@@ -3,6 +3,7 @@ cd /etc/profile.d/
 touch go.sh
 
 export GOROOT=~/Tools/go/default
+export GOPATH=~/Projects/go
 
-echo "export GOROOT=$GOROOT\nexport PATH=$GOROOT/bin:\$PATH\n" > go.sh
+echo "export GOROOT=$GOROOT\nexport GOPATH=$GOPATH\nexport PATH=$GOROOT/bin:\$PATH\n" > go.sh
 
